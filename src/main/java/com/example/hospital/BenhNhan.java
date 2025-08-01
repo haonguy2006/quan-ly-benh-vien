e634748db3069aa875005536185391984c707b8d
haonguyen06 <24100450@st.phenikaa-uni.edu.vn>
2025-08-01 16:42:24 +0700

    Thêm BenhNhanController.java

---
+++ src/main/java/com/example/hospital/BenhNhan.java
@@ -0,0 +1,31 @@
+package com.example.hospital;
+
+public class BenhNhan {
+    private String hoTen;
+    private int tuoi;
+    private String trieuChung;
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getTrieuChung() {
+        return trieuChung;
+    }
+
+    public void setTrieuChung(String trieuChung) {
+        this.trieuChung = trieuChung;
+    }
+}
